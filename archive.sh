256276a38d2aca38fa7661a5763d0371b8a15888
Chielo Newctle <ChieloNewctle@Yandex.com>
2019-11-22 14:07:45 +0800

    update

--- archive.sh
+++ archive.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+zip_prefix=c1-
+
 source problems.sh
 
 for i in $problems; do
     echo $i
+    dest=$zip_prefix$1.zip
     (cd problems/$i \
-        && rm -f $i.zip && zip -r $i.zip $(realpath --relative-to=$PWD \
+        && rm -f $dest && zip -r $dest $(realpath --relative-to=$PWD \
             $(readlink -e problem.pdf problem.yaml domjudge-problem.ini data submissions)) \
     )
 done
